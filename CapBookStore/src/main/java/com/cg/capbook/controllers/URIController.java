2f38ed30976a3cfa0a6504e49f410e910e882fec
ADM-IG-HWDLAB1D <ADM-IG-HWDLAB1D@PUNHWD12264.corp.capgemini.com>
2019-03-20 11:26:25 +0530

    commited by harshpriya :26 20march

--- CapBookStore/src/main/java/com/cg/capbook/controllers/URIController.java
+++ CapBookStore/src/main/java/com/cg/capbook/controllers/URIController.java
@@ -1,5 +1,22 @@
 package com.cg.capbook.controllers;
 
-public class URIController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cg.capbook.beans.User;
+
+@Controller
+public class URIController {
+ 
+	private User user;
+	@RequestMapping(value= {"/","index"})
+   public String getIndexPage() {
+	   return "indexPage";
+   }	
+	@ModelAttribute
+	public User getUser() {
+		user=new User();
+		return user;
+	}
 }
